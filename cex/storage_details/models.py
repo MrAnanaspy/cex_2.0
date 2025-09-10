ee59cd791b4f759054d6595a7ef2102c1033cdb5
DO <stasssssssssssss13@gmail.com>
2025-09-10 16:13:55 +0300

    Создание модели детали, марки материалов и формы материалов

--- cex/storage_details/models.py
+++ cex/storage_details/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 # Create your models here.
+def image_directory_path(instance, filename):
+    return '{0}/img/{1}'.format(instance.EAM, filename)
+
+
+class AllMaterials(models.Model):
+    stamp = models.CharField(max_length=50, verbose_name='Марка материала', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.stamp}"
+
+    class Meta:
+        db_table = "MaterialsAll"
+        verbose_name_plural = 'Марка материала'
+
+class ShapeMaterials(models.Model):
+    shape = models.CharField(max_length=50, verbose_name='Форма материала', primary_key=True)
+
+    def __str__(self):
+        return f"{self.shape}"
+
+    class Meta:
+        db_table = "MaterialsShape"
+        verbose_name_plural = 'Формы материала'
+
+class Detail(models.Model):
+    DIVISION = (
+        ('БФО ', 'БФО'),
+        ('Варочный ', 'Варочный'),
+        ('Розлив ', 'Розлив'),
+        ('Солодовня ', 'Солодовня'),
+        ('Котельня ', 'Котельня'),
+    )
+    EAM = (models.CharField
+           (max_length=20))
+    name = (models.CharField
+            (max_length=100, verbose_name='Название', null=True, blank=True))
+    division = models.CharField(max_length=50, choices=DIVISION, verbose_name='Подразделение', null=True,
+                                           blank=True)
+    mater = models.ForeignKey(AllMaterials, on_delete=models.CASCADE, verbose_name='Марка материала', null=True, blank=True)
+    shape_mater = models.ForeignKey(ShapeMaterials, on_delete=models.CASCADE, verbose_name='Форма материла', null=True, blank=True)
+    billet_weight = (models.CharField
+                (max_length=100, verbose_name='Вес заготовки', null=True, blank=True))
+    photo = (models.ImageField
+             (max_length=260, verbose_name='Ссылка на фото', upload_to=image_directory_path, null=True, blank=True))
+    twt = models.FloatField(max_length=30, verbose_name='Время работы токарного станка на 1 шт (мин)', default=0)
+    twd = models.FloatField(max_length=30, verbose_name='Время простоя токарного станка на 1 шт (мин)', default=0)
+    mwt = models.FloatField(max_length=30, verbose_name='Время работы фрезерного станка на 1 шт (мин)', default=0)
+    mwd = models.FloatField(max_length=30, verbose_name='Время простоя фрезерного станка на 1 шт (мин)', default=0)
+    tmwt = models.FloatField(max_length=30, verbose_name='Время работы токарно-фрезерного станка на 1 шт (мин)', default=0)
+    tmwd = models.FloatField(max_length=30, verbose_name='Время простоя токарно-фрезерного станка на 1 шт (мин)', default=0)
+    ewt = models.FloatField(max_length=30, verbose_name='Время работы электроэррозии на 1 шт (мин)',
+                             default=0)
+    ewd = models.FloatField(max_length=30, verbose_name='Время простоя электроэррозии на 1 шт (мин)',
+                             default=0)
+    procurement_work = models.FloatField(max_length=30,
+                                         verbose_name='Время потраченное на заготовительные операции (Пила, плазма) 1 шт (мин)',
+                                         default=0)
+    locksmith_operations = models.FloatField(max_length=30,
+                                         verbose_name='Время потраченное на слесарные операции 1 шт (мин)', default=0)
+    post_processing = models.FloatField(max_length=30,
+                                         verbose_name='Время потраченное на постобработку 1 шт (мин)', default=0)
+    technical_control = models.FloatField(max_length=30,
+                                        verbose_name='Время потраченное на технический контроль 1 шт (мин)', default=0)
+    packaging = models.FloatField(max_length=30,
+                                          verbose_name='Время потраченное на упаковку 1 шт (мин)', default=0)
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.EAM} - {self.name}"
+
+    class Meta:
+        db_table = "Detail"
+        verbose_name_plural = 'Детали'
+
