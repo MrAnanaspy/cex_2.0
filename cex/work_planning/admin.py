6bb413658c2dc4bbe56c9ac50a49a6eab5169be6
DO <stasssssssssssss13@gmail.com>
2025-09-16 13:16:48 +0300

    создал вывод в админ панель модели планирования работ

--- cex/work_planning/admin.py
+++ cex/work_planning/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
+
+from .models import *
+
 
 # Register your models here.
+@admin.register(Branch)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Signature)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(EngineerWork)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(TechnologistWork)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Machine)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MachineOperation)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(OperatorWork)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Task)
+class DetailAdmin(MPTTModelAdmin):
+    pass
+
+@admin.register(Act)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(MSK)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Application)
+class DetailAdmin(admin.ModelAdmin):
+    pass
