802047c943e161aa7a8a2370525bd37dceb397df
DO <stasssssssssssss13@gmail.com>
2025-09-17 09:36:31 +0300

    редактирование имен

--- cex/economy/admin.py
+++ cex/economy/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Expenses)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Analog)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(WAP)
+class DetailAdmin(admin.ModelAdmin):
+    pass
+
