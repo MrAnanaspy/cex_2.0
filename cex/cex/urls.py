4817b7e0f5f702a17e01dee6e2b05d26ce86bb99
DO <stasssssssssssss13@gmail.com>
2025-10-21 12:12:23 +0300

    Проведена начальная настройка планирования работ

--- cex/cex/urls.py
+++ cex/cex/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('warehouse_materials.urls')),
+    path('', include('work_planning.urls')),
 ]
--- cex/work_planning/views.py
+++ cex/work_planning/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def create_appeal(request):
+    context = {
+        'products': ['Товар 1', 'Товар 2', 'Товар 3'],
+    }
+    return render(request, 'material_storage.html', context)
